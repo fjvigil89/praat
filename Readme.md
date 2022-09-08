4d50b87319fc23e3869af20bdb67fda118d970ab
fjvigil89 <frankjosue.vigilvega@gmail.com>
2022-09-08 09:56:23 +0200

    ajustando ciclo para sacar las muestras

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 
 # BaseLine Pathologies
-* 
+* ya funciona la creación de las listas de json usando los .csv
+* ya funciona la creación de los features usando las listas json y se guardan en .pkl
--- src/app.py
+++ src/app.py
@@ -3,14 +3,12 @@ from Praat import Praat as praat
 from svm_training import main 
 
 
-def feature_smile():
+def feature_smile(): #ok
     main.feature_smile()
 
 ### Esta función es para crear los kfold en base a un metadata.xlsx de la base de datos ###
 def crea_kfold():
     main.crea_list_kfold()
-
-
 
 
 if __name__ == '__main__':
